fdabcab2a28fe98111b61ac4c9de9ac09f4a0faa
Christian <christian.sw@hotmail.de>
2020-09-18 10:28:23 +0200

    Added outputfile path choice to pageselect

--- pageselect.py
+++ pageselect.py
@@ -2,9 +2,9 @@ import fitz
 import sys
 
 doc = fitz.open(sys.argv[1])
-pages = sys.argv[2:]
+pages = sys.argv[2:-1]
 pages = [int(i) for i in pages]
 
 doc.select(pages)
-doc.save("reselect.pdf", 2)
+doc.save(sys.argv[-1], 2)
 doc.close()
