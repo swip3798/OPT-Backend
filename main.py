699be0a63cd16794ae76fc7bd5a94748cfefefda
Christian <christian.sw@hotmail.de>
2021-06-29 18:51:28 +0200

    Fix crash when running backend with pythonw

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+# Needed in order to run this with pythonw.exe
+import sys
+if sys.stdout == None:
+	sys.stdout = open('log.txt', 'w')
+	sys.stderr = open('err.txt', 'w')
 from opt import server
 
 server.execute(port = 43512, server = "tornado")
