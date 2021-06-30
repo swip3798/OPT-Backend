d367b92d15e414829458056eadd352855de7207f
Christian <christian.sw@hotmail.de>
2021-06-30 16:57:31 +0200

    Potentially fix overwriting issues in page select

--- opt/page_select.py
+++ opt/page_select.py
@@ -1,4 +1,5 @@
 from . import server
+import os
 import fitz
 
 @server.command
@@ -6,8 +7,9 @@ def select_pages(data):
     doc = fitz.open(data["input_file"])
     page_sequence = data["pages"]
     doc.select(page_sequence)
-    doc.save(data["output_file"], garbage = 3)
+    doc.save(data["output_file"] + "_tmp", garbage = 3, )
     doc.close()
+    os.rename(data["output_file"] + "_tmp", data["output_file"])
     return {
         "output_file": data["output_file"]
     }
