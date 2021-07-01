47027176494ac1686e8c714b0d176af01c5d5fc2
Christian Schweigel <christian.david.schweigel@sap.com>
2021-07-01 13:25:25 +0200

    Add post build script to clear up build

---
+++ post_build.bat
@@ -0,0 +1,9 @@
+del /Q *.pdf
+del /Q *.jpg
+del /Q *.jpeg
+del /Q post_build.bat
+rmdir /Q /S pip
+rmdir /Q /S pip-21.1.2.dist-info
+rmdir /Q /S setuptools
+rmdir /Q /S setuptools-41.2.0.dist-info
+rmdir /Q /S tornado\test
