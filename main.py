150050e897e3fad03a21063b1e4948951639f893
Christian <christian.sw@hotmail.de>
2021-07-03 13:14:59 +0200

    Change stdout stream to devnull to work in ProgramsFolder of windows

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 # Needed in order to run this with pythonw.exe
 import sys
+import os
 if sys.stdout == None:
-	sys.stdout = open('log.txt', 'w')
-	sys.stderr = open('err.txt', 'w')
+	sys.stdout = open(os.devnull, 'w')
+	sys.stderr = open(os.devnull, 'w')
 from opt import server
 
 server.execute(port = 43512, server = "tornado")
